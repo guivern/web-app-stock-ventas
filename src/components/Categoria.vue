ce3b226bda400fb7e63e0fb443792fd3f332eb5e
Guille <guille@localhost.localdomain>
2019-01-24 21:42:22 -0300

    form de categorias

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -5,16 +5,17 @@
         <v-toolbar-title>Categorías</v-toolbar-title>
         <v-divider class="mx-2" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-text-field class="text-xs-center" 
-            v-model="search"
-            append-icon="search" 
-            label="Búsqueda"
-            single-line
-            hide-details>
-        </v-text-field>
+        <v-text-field
+          class="text-xs-center"
+          v-model="search"
+          append-icon="search"
+          label="Búsqueda"
+          single-line
+          hide-details
+        ></v-text-field>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
-          <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
+          <v-btn slot="activator" color="primary" dark class="mb-2">Nuevo</v-btn>
           <v-card>
             <v-card-title>
               <span class="headline">{{ formTitle }}</span>
@@ -23,20 +24,11 @@
             <v-card-text>
               <v-container grid-list-md>
                 <v-layout wrap>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                  <v-flex xs12 sm12 md12>
+                    <v-text-field v-model="categoria.nombre" label="Nombre" :error-messages="mensajeValidacion['Nombre']"></v-text-field>
                   </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                  <v-flex xs12 sm12 md12>
+                    <v-text-field v-model="categoria.descripcion" label="Descripcion"></v-text-field>
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -44,32 +36,47 @@
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+              <v-btn color="blue darken-1" flat @click="close">Cancelar</v-btn>
+              <v-btn color="blue darken-1" flat @click="guardar" :loading="guardando">Guardar</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
       </v-toolbar>
-      <v-data-table 
-        :headers="headers" 
-        :items="desserts" 
+      <v-data-table
+        :headers="headers"
+        :items="categorias"
         class="elevation-1"
-        :search="search">
+        :search="search"
+        :loading="cargando"
+      >
         <template slot="items" slot-scope="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="justify-center layout px-0">
+          <td>
             <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
             <v-icon small @click="deleteItem(props.item)">delete</v-icon>
           </td>
+          <td>{{ props.item.nombre }}</td>
+          <td>{{ props.item.descripcion }}</td>
+          <td
+            :class="{'indigo--text':props.item.activo, 'blue-grey--text':!props.item.activo}"
+          >{{ props.item.activo ? 'Activo' : 'Inactivo' }}</td>
         </template>
         <template slot="no-data">
-          <v-btn color="primary" @click="initialize">Reset</v-btn>
+          <v-btn color="primary" @click="initialize">Resetear</v-btn>
         </template>
       </v-data-table>
+      <!--<v-btn
+        fixed
+        dark
+        fab
+        bottom
+        right
+        type="button"
+        title="Nuevo"
+        color="primary"
+        @click="dialog = true"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>-->
     </v-flex>
   </v-layout>
 </template>
@@ -78,118 +85,47 @@
 export default {
   data() {
     return {
+      id: null,
+      categorias: [],
       dialog: false,
+      cargando: false,
+      guardando: false,
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name"
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Actions", value: "name", sortable: false }
+        { text: "Opciones", value: "opciones", sortable: false },
+        { text: "Nombre", value: "nombre" },
+        { text: "Descripcion", value: "descripcion", sortable: false },
+        { text: "Estado", value: "activo" }
       ],
-      desserts: [],
-      editedIndex: -1,
-      editedItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+      categoria: {
+        nombre: "",
+        descripcion: ""
       },
-      defaultItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+      categoriaDefault: {
+        nombre: "",
+        descripcion: ""
       },
-      search: '',
+      search: "",
+      mensajeValidacion: [],
     };
   },
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ];
+    listar() {
+      this.cargando = true;
+      this.$http
+        .get(`${process.env.VUE_APP_ROOT_API}categorias`)
+        .then(response => {
+          this.categorias = response.data;
+          this.cargando = false;
+        })
+        .catch(function(error) {
+          console.log(error);
+          this.cargando = false;
+        });
     },
-
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+      this.id = item.id;
+      this.categoria.nombre = item.nombre;
+      this.categoria.descripcion = item.descripcion;
       this.dialog = true;
     },
 
@@ -202,23 +138,52 @@ export default {
     close() {
       this.dialog = false;
       setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
+        this.limpiar();  
       }, 300);
     },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+    guardar() {
+      this.guardando = true;
+      if (this.id) {
+        // Editar
+        this.$http
+          .put(`${process.env.VUE_APP_ROOT_API}categorias/${this.id}`, this.categoria)
+          .then(response => {
+            this.guardando = false;
+            this.close();
+            this.listar();
+            this.limpiar();
+          })
+          .catch(error => {
+            this.guardando = false;
+            this.mensajeValidacion = error.response.data.errors 
+          });
       } else {
-        this.desserts.push(this.editedItem);
+        // Guardar
+        this.$http
+          .post(`${process.env.VUE_APP_ROOT_API}categorias`, this.categoria)
+          .then(response => {
+            this.guardando = false;
+            this.close();
+            this.listar();
+            this.limpiar();
+          })
+          .catch(error => {
+            this.guardando = false;
+            this.mensajeValidacion = error.response.data.errors 
+          });
       }
-      this.close();
-    }
+    },
+    limpiar() {
+      this.categoria = Object.assign({}, this.categoriaDefault);
+      this.mensajeValidacion = [];
+      this.id = null;
+    },
   },
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+      return this.id
+        ? "Nueva categoria"
+        : "Actualizar categoria";
     }
   },
 
@@ -229,7 +194,7 @@ export default {
   },
 
   created() {
-    this.initialize();
+    this.listar();
   }
 };
 </script>
