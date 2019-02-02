fd42dd5d07e846b79dc186b42a6520157abd1450
Guille <guille@localhost.localdomain>
2019-02-02 17:00:09 -0300

    se agrega proveedores a la navegacion

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@
                 <v-list-tile-title>Ingresos</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <v-list-tile :to="{name: ''}">
+            <v-list-tile :to="{name: 'proveedores'}">
               <v-list-tile-action>
                 <v-icon>table_chart</v-icon>
               </v-list-tile-action>
