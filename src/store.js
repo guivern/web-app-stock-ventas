2d431127f36d024f69ddfe2d1f3c39212a396dd8
Guille <guille@localhost.localdomain>
2019-02-03 18:31:03 -0300

    se almacena el token y el rol del usuario logueado

--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import decode from 'jwt-decode'
+import router from './router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    token: null,
+    usuario: null
   },
   mutations: {
-
+    // logica para cambiar de estado
+    setToken(state, token) {
+      state.token = token
+    },
+    setUsuario(state, usuario) {
+      state.usuario = usuario
+    }
   },
   actions: {
-
+    // logica para realizar mutaciones
+    guardarToken({commit}, token) {
+      //almacena el token del usuario logeado
+      commit("setToken", token)
+      commit("setUsuario", decode(token))
+      localStorage.setItem("token", token)
+    },
+    autoLogin({commit}) {
+      let token = localStorage.getItem("token")
+      if (token) {
+        commit("setToken", token)
+        commit("setUsuario", decode(token))
+      }
+      router.push({ name: 'home' })
+    },
+    salir({commit}) {
+      commit("setToken", null)
+      commit("setUsuario", null)
+      localStorage.removeItem("token")
+      router.push({name: 'login'})
+    }
   }
 })
